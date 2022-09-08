d475095d96598b0a29e11c4cb564f73faf1528ff
jayjeong8 <zuni4242@gmail.com>
2022-09-08 21:47:51 +0900

    refactor : Prettier 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,61 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const webpack = require("webpack");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  mode: "development",
+    mode: 'development',
 
-  entry: {
-    app: './src/app.js',
-  }, output: {
-    path: path.resolve(__dirname, 'dist'), // publicPath: "/",
-    filename: 'bundle.js',
-  },
+    entry: {
+        app: './src/app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'), // publicPath: "/",
+        filename: 'bundle.js',
+    },
 
-  devtool: 'inline-source-map', devServer: {
-    static: './dist', hot: true,
-  },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
 
-  module: {
-    rules: [{
-      test: /\.js$/, exclude: /node_modules/, use: {
-        loader: 'babel-loader'
-      }
-    }, {
-      test: /\.css$/i, use: ["style-loader", "css-loader", "sass-loader"],
-    }, {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource',
-    }, {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource',
-    },],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 
-  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
-    template: 'src/app.html'
-  }), new webpack.HotModuleReplacementPlugin()], resolve: {
-    alias: {
-      '@css': path.resolve(__dirname, 'src/'), //경로 바꾸기
-      '@js': path.resolve(__dirname, 'src/'), '@assets': path.resolve(__dirname, 'src/'),
-    }
-  }
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/app.html',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    resolve: {
+        alias: {
+            '@css': path.resolve(__dirname, 'src/'), //경로 바꾸기
+            '@js': path.resolve(__dirname, 'src/'),
+            '@assets': path.resolve(__dirname, 'src/'),
+        },
+    },
 };
