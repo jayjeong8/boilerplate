d693a41017e8bde61104333c5e6f98ed6dd7f38a
jayjeong8 <zuni4242@gmail.com>
2022-09-08 21:36:58 +0900

    refactor : 웹팩 환경설정 추가

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
+import './style.css';
 import MainService from './mainService.js';
-
+import Barcode from './barcode.svg'
 const targetEl = document.querySelector("#log");
 
 const service = new MainService({targetEl})
@@ -7,3 +8,12 @@ const datalist = [1,2,3,4,[5,6,[7]]];
 const subHtml = service.init(datalist);
 
 targetEl.innerHTML = `datalist is ${subHtml}`;
+
+// Add the image to our existing div.
+const element = document.createElement('div');
+const myIcon = new Image();
+myIcon.src = Barcode;
+
+element.appendChild(myIcon);
+
+document.body.appendChild(element);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const webpack = require("webpack");
 
 module.exports = {
@@ -8,43 +8,35 @@ module.exports = {
 
   entry: {
     app: './src/app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    // publicPath: "/",
+  }, output: {
+    path: path.resolve(__dirname, 'dist'), // publicPath: "/",
     filename: 'bundle.js',
   },
 
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    hot: true ,
+  devtool: 'inline-source-map', devServer: {
+    static: './dist', hot: true,
   },
 
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-
-    ],
+    rules: [{
+      test: /\.js$/, exclude: /node_modules/, use: {
+        loader: 'babel-loader'
+      }
+    }, {
+      test: /\.css$/i, use: ["style-loader", "css-loader", "sass-loader"],
+    }, {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource',
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource',
+    },],
   },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin(
-    {
-      template: 'src/app.html'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({
+    template: 'src/app.html'
+  }), new webpack.HotModuleReplacementPlugin()], resolve: {
+    alias: {
+      '@css': path.resolve(__dirname, 'src/'), //경로 바꾸기
+      '@js': path.resolve(__dirname, 'src/'), '@assets': path.resolve(__dirname, 'src/'),
+    }
+  }
 };
